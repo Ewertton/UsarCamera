73e9b6212902d7490733e9243a9778f7df3aba45
Ewertton <ewertton.kaytton@hotmail.com>
2015-10-07 20:13:54 -0300

    1.0.0.0

---
+++ UsarCamera/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace UsarCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.leitura = new System.Windows.Forms.Button();
+            this.parar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(41, 46);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(297, 250);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(304, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // leitura
+            // 
+            this.leitura.Location = new System.Drawing.Point(387, 10);
+            this.leitura.Name = "leitura";
+            this.leitura.Size = new System.Drawing.Size(75, 23);
+            this.leitura.TabIndex = 3;
+            this.leitura.Text = "ler";
+            this.leitura.UseVisualStyleBackColor = true;
+            this.leitura.Click += new System.EventHandler(this.leitura_Click);
+            // 
+            // parar
+            // 
+            this.parar.Location = new System.Drawing.Point(492, 10);
+            this.parar.Name = "parar";
+            this.parar.Size = new System.Drawing.Size(75, 23);
+            this.parar.TabIndex = 4;
+            this.parar.Text = "parar";
+            this.parar.UseVisualStyleBackColor = true;
+            this.parar.Click += new System.EventHandler(this.parar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(387, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(247, 212);
+            this.listBox1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 321);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.parar);
+            this.Controls.Add(this.leitura);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button leitura;
+        private System.Windows.Forms.Button parar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ UsarCamera/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using BarcodeLib.BarcodeReader;
+
+namespace UsarCamera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private FilterInfoCollection Dispositivos;
+
+        private VideoCaptureDevice Video;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // lista dispositio com entrada e video
+
+            Dispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach(FilterInfo x in Dispositivos)
+            {
+                comboBox1.Items.Add(x.Name);
+            }
+
+            comboBox1.SelectedIndex = 0; 
+        }
+
+        private void leitura_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = true;
+            Video = new VideoCaptureDevice(Dispositivos[comboBox1.SelectedIndex].MonikerString);
+            videoSourcePlayer1.VideoSource = Video;
+            videoSourcePlayer1.Start();
+
+        }
+
+        private void parar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            videoSourcePlayer1.SignalToStop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (videoSourcePlayer1.GetCurrentVideoFrame() != null)
+            {
+                Bitmap img = new Bitmap(videoSourcePlayer1.GetCurrentVideoFrame());
+                string[] resultado = BarcodeReader.read(img, BarcodeReader.QRCODE);
+                img.Dispose();
+
+                if(resultado!= null && resultado.Count() > 0)
+                {
+                    listBox1.Items.Add(resultado[0]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.listBox1.DataSource = null;
+        }
+    }
+}
